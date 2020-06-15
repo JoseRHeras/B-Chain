b8238f809b5b1b402f5549aaccd5b6fe05e35d0c
JRHeras <jheras@yahoo.com>
2020-06-15 15:48:41 -0500

    Blockchain class created

---
+++ blockchain.py
@@ -0,0 +1,18 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+    
+    def new_block(self):
+        # Adds a new transaction to the list of transactions
+        pass
+
+    @staticmethod
+    def hash(block):
+        #Hashes a Block
+        pass
+
+    @property
+    def last_block(self):
+        #Returns the last Block in the chain
+        pass
